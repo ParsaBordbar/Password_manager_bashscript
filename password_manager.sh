f1847f707351a34902f730e9c9253608bcc3ad21
Parsa Bordbar <124056966+ParsaBordbar@users.noreply.github.com>
2024-01-26 20:55:04 +0330

    Update password_manager.sh

--- password_manager.sh
+++ password_manager.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Clear the terminal
 clear
 
-# Font Variations
-bold="\e[1m"
-rbold="\e[22m"
-italic="\e[3m"
-ritalic="\e[23m"
+#Styles:
 
-# Colors
-yellowText="\e[38;2;255;255;0m"
-yellowBg="\e[48;2;255;255;0m"
-blackText="\e[38;2;0;0;0m"
-blackBg="\e[48;2;0;0;0m"
-reset="\e[m"
+	#Font Variations
+	bold="\e[1m"
+	rbold="\e[22m"
+@@ -17,6 +14,15 @@ clear
+	blackBg="\e[48;2;0;0;0m"
+	reset="\e[m"
 
-# Display the banner
+
 echo -e "${yellowText} 
  ____                                     _
 |  _ \ __ _ ___ _____      _____  _ __ __| |    _ __ ___   __ _ _ __   __ _  __ _  ___ _ __
@@ -25,87 +20,85 @@ echo -e "${yellowText}
 |_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|___|_| |_| |_|\__,_|_| |_|\__,_|\__, |\___|_|
                                           |_____|                           |___/${reset}
 "
-
-# This is the database of our project:
+#This is the database of our project:
 password_database="password_database.txt"
 
-# Create the database file if it doesn't exist
-if [ ! -e "$password_database" ]
-then 
-	touch "$password_database"
-fi
-
-# Secure the database file
-chmod 600 "$password_database"
-
-# Function to add a new password
+@@ -28,48 +34,57 @@ fi
+#Makes new password
 add_new_password(){
 	clear
 	echo -e "${blackBg}${yellowText}${bold}Adding new password${rbold}${reset}${reset}\n"
 	read -p "Enter the account name: " account 
-	read -sp "Enter the password: " password
-	encoded_password=$(echo -n "$password" | base64)
-	echo "$account: $encoded_password" >> "$password_database"
-	echo -e "\nThe account $account was added successfully.\n"
+	read -p "Enter the password: " password
+	echo "$account: $password" >> "$password_database"
+	echo "The account $account was added succsesfuly."
+	echo -e "\nThe account $account was added succsesfuly.\n"
 }
 
-# Function to retrieve a password
+#Gets the password
 get_password(){
 	clear
-	echo -e "${blackBg}${yellowText}${bold}Retrieving password${rbold}${reset}${reset}\n"
-	read -p "Enter the account name: " account
-	encoded_password=$(grep "^$account:" "$password_database" | cut -d ":" -f 2)
-	if [ -n "$encoded_password" ]; 
+	echo -e "${blackBg}${yellowText}${bold}Retriveing password${rbold}${reset}${reset}\n"
+        read -p "Enter the account name: " account
+	password=$(grep "^$account:" "$password_database" | cut -d " " -f 2)
+
+	if [ -n "$password" ]; 
 	then
-		decoded_password=$(echo -n "$encoded_password" | base64 --decode)
-        	echo -e "\nPassword for $account:\n ${yellowText} $decoded_password ${reset}\n"
-	else
+        	echo "Password for $account: $password"
+        	echo -e "\n${italic}Password for $account${ritalic}:\n ${yellowText} $password ${reset}\n"
+        else
+        	echo "Password not found for $account."
         	echo -e "\nPassword not found for $account."
-	fi
+    fi
 }
 
-
-# Function to generate a password
+#Generate a random pass word with length of 12
 generate_password(){
 	clear
-	echo -e "${blackBg}${yellowText}${bold}Generate Password${rbold}${reset}${reset}\n"
+	echo -e "${blackBg}${yellowText}${bold}Genrate Password${rbold}${reset}${reset}\n"
 	read -p "Enter the account name you want to generate a password for: " account
 	length=12
 	password=$(tr -dc 'a-zA-Z0-9@#$%^!~()' < /dev/urandom | fold -w ${length} | head -n 1)
+	 echo "$password added for account ${account}"
+	 echo "${yellowBg}$account: $password" >> "$password_database${reset}"
 	echo -e "${yellowText}$password${reset} ${italic}added as password for account ${account}\n ${ritalic}"
 	echo -e "${yellowBg}$account: $password" >> "$password_database ${reset}"
 }
 
-# Function to delete a password
-delete_password(){
-	clear
-	echo -e "${blackBg}${yellowText}${bold}Deleting password${rbold}${reset}${reset}\n"
-	read -p "Enter the account name: " account
-	grep -v "^$account:" "$password_database" > "temp.txt" && mv "temp.txt" "$password_database"
-	echo -e "\nThe password for account $account was deleted successfully.\n"
-}
-
-# Main menu function
+#Here we are Making a Menu for it
+while true;
+do
+	echo -e "\n${bold}Password mangaer${rbold}"
+	echo -e "1-Add new password"
+	echo -e "2-Retrive password"
+	echo -e "3-Generate password"
+	echo -e "4-Quit" 
+	read -p "Select an item: " choice 
+	case $choice in 
+		1) add_new_password;;
+		2) get_password;;
+		3) generate_password;;
+		4) exit;;
+		*) echo "Invalid Option"
+	esac
+done
 main_menu(){
 	while true;
 	do
-		echo -e "${blackBg}${yellowText}${bold}Password manager menu${rbold}${reset}${reset}\n"
+		echo -e "${blackBg}${yellowText}${bold}Password mangaer menu${rbold}${reset}${reset}\n"
 		echo -e "1-Add new password"
-		echo -e "2-Retrieve password"
+		echo -e "2-Retrive password"
 		echo -e "3-Generate password"
-		echo -e "4-Delete password"
-		echo -e "5-Quit\n"
+		echo -e "4-Quit\n"
         	read -p "Select an option: " choice	
 		case $choice in 
 			1) add_new_password;;
 			2) get_password;;
 			3) generate_password;;
-			4) delete_password;;
-			5) exit;;
+			4) exit;;
 			*) echo "Invalid Option"
 		esac
 	done
 }
 
-# Call the main menu function
 main_menu
